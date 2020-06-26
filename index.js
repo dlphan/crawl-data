75b1527e949cbc872fbb3053dfc4a459cf83d8c3
Lam <dlphan.qn@outlook.com>
2020-06-26 16:04:31 +0700

    request

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const axios = require('axios')
-const jsom = require('jsdom')
 
+const MAX_PAGE = 2
+const LIMIT = 500
+
+const crawl = async () => {
+  const request = Array.from(
+    { length: MAX_PAGE },
+    (_, i) => axios({
+      method: 'get',
+      url: `https://www.huffpost.com/api/department/news/cards?page=${i + 1}&limit=${LIMIT}`,
+    })
+  )
+
+  const result = axios
+    .all(request)
+    .then(axios.spread((...responses) => {
+      const page = responses.map(({ data: { cards } }) => cards.map(card => card.headlines[0].text))
+      console.log(page)
+    }))
+}
+
+crawl()
