d2e2bc6c06f758cb941ee958b4a5ef5a4c0fa085
Lam <dlphan.qn@outlook.com>
2020-06-26 16:40:49 +0700

    done

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-const axios = require('axios')
+const axios = require("axios");
+const fs = require('fs')
 
-const MAX_PAGE = 2
-const LIMIT = 500
+const MAX_PAGE = 2;
+const LIMIT = 500;
 
 const crawl = async () => {
-  const request = Array.from(
-    { length: MAX_PAGE },
-    (_, i) => axios({
-      method: 'get',
-      url: `https://www.huffpost.com/api/department/news/cards?page=${i + 1}&limit=${LIMIT}`,
+  const request = Array.from({ length: MAX_PAGE }, (_, i) =>
+    axios({
+      method: "get",
+      url: `https://www.huffpost.com/api/department/news/cards?page=${
+        i + 1
+      }&limit=${LIMIT}`,
     })
-  )
+  );
 
-  const result = axios
-    .all(request)
-    .then(axios.spread((...responses) => {
-      const page = responses.map(({ data: { cards } }) => cards.map(card => card.headlines[0].text))
-      console.log(page)
-    }))
-}
+  const result = await axios.all(request).then(
+    axios.spread((...responses) => {
+      const page = responses.map(({ data: { cards } }) =>
+        cards.map((card) => 
+          ({
+            is_sarcastic: 0,
+            headline: card.headlines[0].text,
+            article_link: card.headlines[0].url
+          }))
+      );
+      return [].concat(...page);
+    })
+  );
 
-crawl()
+  fs.writeFileSync('./data/huffpost.json', JSON.stringify(result), 'utf-8')
+};
+
+crawl();
